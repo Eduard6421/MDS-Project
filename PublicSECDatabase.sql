05d9addecaae7ca7d9af92f2cdd4a609126a2926
Eduard6421 <eduard_poe2000@yahoo.com>
2018-04-17 23:03:58 +0300

    Added UI Sketches ( Desktop + Mobile), redesigned Database

--- PublicSECDatabase.sql
+++ PublicSECDatabase.sql
@@ -1,72 +1,81 @@
 CREATE TABLE `Clients` (
-	`ID` bigint NOT NULL AUTO_INCREMENT,
-	`First_Name` varchar NOT NULL AUTO_INCREMENT,
-	`Last_Name` varchar NOT NULL AUTO_INCREMENT,
-	`User_Name` varchar NOT NULL AUTO_INCREMENT,
-	`User_Password` varchar NOT NULL AUTO_INCREMENT,
-	`Address` varchar NOT NULL AUTO_INCREMENT,
-	`Phone` varchar NOT NULL AUTO_INCREMENT,
-	`Email` varchar AUTO_INCREMENT,
+	`ID` bigint NOT NULL,
+	`First_Name` varchar NOT NULL,
+	`Last_Name` varchar NOT NULL,
+	`User_Name` varchar NOT NULL,
+	`User_Password` varchar NOT NULL,
+	`Address` varchar NOT NULL,
+	`Phone` varchar NOT NULL,
+	`Email` varchar,
 	PRIMARY KEY (`ID`)
 );
 
 CREATE TABLE `Company` (
-	`ID` int NOT NULL AUTO_INCREMENT,
-	`Name` varchar NOT NULL AUTO_INCREMENT,
-	`Contract_Start_Date` DATE NOT NULL AUTO_INCREMENT,
-	`Contract_End_Date` DATE NOT NULL AUTO_INCREMENT,
-	`Description` varchar AUTO_INCREMENT,
+	`ID` int NOT NULL,
+	`Name` varchar NOT NULL,
+	`Contract_Start_Date` DATE NOT NULL,
+	`Contract_End_Date` DATE NOT NULL,
+	`Description` varchar,
 	PRIMARY KEY (`ID`)
 );
 
 CREATE TABLE `Employee` (
-	`ID` int NOT NULL AUTO_INCREMENT,
-	`Username` varchar NOT NULL AUTO_INCREMENT,
-	`Password` varchar NOT NULL AUTO_INCREMENT,
-	`First_Name` varchar NOT NULL AUTO_INCREMENT,
-	`Last_Name` varchar NOT NULL AUTO_INCREMENT,
-	`Phone` varchar NOT NULL AUTO_INCREMENT,
-	`Email` varchar NOT NULL AUTO_INCREMENT,
+	`ID` int NOT NULL,
+	`Username` varchar NOT NULL,
+	`Password` varchar NOT NULL,
+	`First_Name` varchar NOT NULL,
+	`Last_Name` varchar NOT NULL,
+	`Phone` varchar NOT NULL,
+	`Email` varchar NOT NULL,
 	PRIMARY KEY (`ID`)
 );
 
 CREATE TABLE `Client_Contract` (
-	`ID` int NOT NULL AUTO_INCREMENT,
-	`ID_Client` int NOT NULL AUTO_INCREMENT,
-	`ID_Company` int NOT NULL AUTO_INCREMENT,
+	`ID` int NOT NULL,
+	`ID_Company`  NOT NULL,
+	`ID_Client`  NOT NULL,
 	`Start_Date` DATE NOT NULL,
 	`End_Date` DATE NOT NULL,
 	PRIMARY KEY (`ID`)
 );
 
 CREATE TABLE `Employee_Contract` (
-	`ID` int NOT NULL AUTO_INCREMENT,
-	`ID_Company` int NOT NULL AUTO_INCREMENT,
-	`ID_Employee` int NOT NULL AUTO_INCREMENT,
+	`ID` int NOT NULL,
+	`ID_Company`  NOT NULL,
+	`ID_Employee`  NOT NULL,
 	`Start_Date` DATE NOT NULL,
 	`End_Date` DATE NOT NULL,
 	PRIMARY KEY (`ID`)
 );
 
-CREATE TABLE `Logs` (
-	`ID` bigint NOT NULL AUTO_INCREMENT,
-	`ID_Client` int NOT NULL,
-	`ID_Employee` int NOT NULL,
+CREATE TABLE `Meetings` (
+	`ID` bigint NOT NULL,
+	`ID_Client`  NOT NULL,
+	`ID_Employee`  NOT NULL,
 	`Date` DATE NOT NULL,
 	`Feedback` int,
 	`Description` mediumtext,
 	PRIMARY KEY (`ID`)
 );
 
-ALTER TABLE `Client_Contract` ADD CONSTRAINT `Client_Contract_fk0` FOREIGN KEY (`ID_Client`) REFERENCES `Clients`(`ID`)ON DELETE CASCADE;
+CREATE TABLE `Reports` (
+	`ID` bigint NOT NULL,
+	`ID_Meeting`  NOT NULL,
+	`Description` TEXT,
+	PRIMARY KEY (`ID`)
+);
 
-ALTER TABLE `Client_Contract` ADD CONSTRAINT `Client_Contract_fk1` FOREIGN KEY (`ID_Company`) REFERENCES `Company`(`ID`) ON DELETE CASCADE;
+ALTER TABLE `Client_Contract` ADD CONSTRAINT `Client_Contract_fk0` FOREIGN KEY (`ID_Company`) REFERENCES `Company`(`ID`);
 
-ALTER TABLE `Employee_Contract` ADD CONSTRAINT `Employee_Contract_fk0` FOREIGN KEY (`ID_Company`) REFERENCES `Company`(`ID`) ON DELETE CASCADE;
+ALTER TABLE `Client_Contract` ADD CONSTRAINT `Client_Contract_fk1` FOREIGN KEY (`ID_Client`) REFERENCES `Clients`(`ID`);
 
-ALTER TABLE `Employee_Contract` ADD CONSTRAINT `Employee_Contract_fk1` FOREIGN KEY (`ID_Employee`) REFERENCES `Employee`(`ID`) ON DELETE CASCADE;
+ALTER TABLE `Employee_Contract` ADD CONSTRAINT `Employee_Contract_fk0` FOREIGN KEY (`ID_Company`) REFERENCES `Company`(`ID`);
 
-ALTER TABLE `Logs` ADD CONSTRAINT `Logs_fk0` FOREIGN KEY (`ID_Client`) REFERENCES `Clients`(`ID`) ON DELETE CASCADE;
+ALTER TABLE `Employee_Contract` ADD CONSTRAINT `Employee_Contract_fk1` FOREIGN KEY (`ID_Employee`) REFERENCES `Employee`(`ID`);
 
-ALTER TABLE `Logs` ADD CONSTRAINT `Logs_fk1` FOREIGN KEY (`ID_Employee`) REFERENCES `Employee`(`ID`) ON DELETE CASCADE;
+ALTER TABLE `Meetings` ADD CONSTRAINT `Meetings_fk0` FOREIGN KEY (`ID_Client`) REFERENCES `Clients`(`ID`);
+
+ALTER TABLE `Meetings` ADD CONSTRAINT `Meetings_fk1` FOREIGN KEY (`ID_Employee`) REFERENCES `Employee`(`ID`);
+
+ALTER TABLE `Reports` ADD CONSTRAINT `Reports_fk0` FOREIGN KEY (`ID_Meeting`) REFERENCES `Meetings`(`ID`);
 
