2d227b83aa5b3b2e907616de7f413d3a1b8d3fc9
StefanIordache <iordachestefan1@gmail.com>
2018-05-15 13:21:24 +0300

    LoginController updates

--- UI Desktop/src/Forms/Controllers/LoginController.java
+++ UI Desktop/src/Forms/Controllers/LoginController.java
@@ -11,6 +11,8 @@ public class LoginController implements ActionListener {
     
     private Login form;
     
+    private boolean focus = true;
+    
     public LoginController() {
         form = new Login(this);
         form.setVisible(true);
@@ -21,30 +23,28 @@ public class LoginController implements ActionListener {
         String username = form.getUsername();
         String password = form.getPassword();
         
-        if(Controllers.LoginController.connectCompany(username,password))
-        {     
-        }
-        else
-        {
-            System.out.println("There is no such user / Database connectivity issues!");
-                    final JFrame popup = new JFrame();
-
-            JButton button = new JButton();
-            button.setText("Wrong credentials or database connectivity issues. Try again!");
-
-            popup.setPreferredSize(new Dimension(500, 200));
-            popup.add(button);
-            popup.pack();
-            popup.setVisible(true);
-
-            button.addActionListener(new java.awt.event.ActionListener() {
-                @Override
-                public void actionPerformed(java.awt.event.ActionEvent evt) {
-                    popup.setVisible(false);
-                    popup.dispose();
-                }
-            });
-        }     
+        String command = evt.getActionCommand();
+        
+        if (focus) {
+            switch (command) {
+                case "Connect as Employee":
+                    if (Controllers.LoginController.connectEmployee(username, password)) {
+                        System.out.println("Angajat");
+                    }
+                    else {
+                        displayConnectionError();
+                    }
+                    break;
+                case "Connect as Company":
+                    if (Controllers.LoginController.connectCompany(username, password)) {
+                        System.out.println("Companie");
+                    }
+                    else {
+                        displayConnectionError();
+                    }
+                    break;
+            }
+        }   
     } 
     
     public void  setWindowInvisible()
@@ -60,5 +60,30 @@ public class LoginController implements ActionListener {
     {
         form.setVisible(false);
         form.dispose();
+    }
+    
+    public void toggleFocus() {
+        focus = !focus;
+    }
+    
+    public void displayConnectionError() {
+        System.out.println("There is no such user / Database connectivity issues!");
+        final JFrame popup = new JFrame();
+
+        JButton button = new JButton();
+        button.setText("Wrong credentials or database connectivity issues. Try again!");
+
+        popup.setPreferredSize(new Dimension(500, 200));
+        popup.add(button);
+        popup.pack();
+        popup.setVisible(true);
+
+        button.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                popup.setVisible(false);
+                popup.dispose();
+            }
+        });
     }
 }
