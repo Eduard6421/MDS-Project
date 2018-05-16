9d4406c43e8bc5a4a1de6c7f96629c00a85272d3
StefanIordache <iordachestefan1@gmail.com>
2018-05-16 11:22:51 +0300

    UPDATE DATABASE

--- Documentation/PublicSECDatabase.sql
+++ Documentation/PublicSECDatabase.sql
@@ -1,237 +1,321 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: publicsec
--- ------------------------------------------------------
--- Server version	5.7.21-log
+-- Host: 127.0.0.1
+-- Generation Time: May 16, 2018 at 10:18 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `client`
+-- Database: `publicsec`
 --
 
-DROP TABLE IF EXISTS `client`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `client` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `First_Name` varchar(50) NOT NULL,
-  `Last_Name` varchar(50) NOT NULL,
-  `User_Name` varchar(225) DEFAULT NULL,
-  `User_Password` varchar(225) DEFAULT NULL,
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE `clients` (
+  `Id` int(11) NOT NULL,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
+  `Username` varchar(225) DEFAULT NULL,
+  `Password` varchar(225) DEFAULT NULL,
   `Address` varchar(50) NOT NULL,
   `Phone` varchar(50) NOT NULL,
-  `Email` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `Email` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `client`
+-- Dumping data for table `clients`
 --
 
-LOCK TABLES `client` WRITE;
-/*!40000 ALTER TABLE `client` DISABLE KEYS */;
-INSERT INTO `client` VALUES (1,'NumeClient1','PrenumeClient1','client1','parolaclient1','Strada Clientului Nr.7','0711223344','client@hotmail.com'),(2,'NumeClient1','PrenumeClient2','client2','parolaclient2','Strada Clientului nr 2','07112234','client2@hotmail.com'),(3,'NumeClient3','PrenumeClient3','client3','parolaclient3','Strada Clientului nr 3','04112341','client3@hotmail.com'),(19,'1','1','1','1','1','1','1');
-/*!40000 ALTER TABLE `client` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `clients` (`Id`, `FirstName`, `LastName`, `Username`, `Password`, `Address`, `Phone`, `Email`) VALUES
+(1, 'NumeClient1', 'PrenumeClient1', 'client1', 'parolaclient1', 'Strada Clientului Nr.7', '0711223344', 'client@hotmail.com'),
+(2, 'NumeClient1', 'PrenumeClient2', 'client2', 'parolaclient2', 'Strada Clientului nr 2', '07112234', 'client2@hotmail.com'),
+(3, 'NumeClient3', 'PrenumeClient3', 'client3', 'parolaclient3', 'Strada Clientului nr 3', '04112341', 'client3@hotmail.com'),
+(19, '1', '1', '1', '1', '1', '1', '1');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `client_contract`
+-- Table structure for table `client_contracts`
 --
 
-DROP TABLE IF EXISTS `client_contract`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `client_contract` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `ID_Company` int(11) NOT NULL,
-  `ID_Client` int(11) NOT NULL,
-  `Start_Date` date NOT NULL,
-  `End_Date` date NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `Company_Foreign_Key_idx` (`ID_Company`),
-  KEY `Client_Foreign_Key_idx` (`ID_Client`),
-  CONSTRAINT `Client_Foreign_Key` FOREIGN KEY (`ID_Client`) REFERENCES `client` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `Company_Foreign_Key` FOREIGN KEY (`ID_Company`) REFERENCES `company` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `client_contracts` (
+  `Id` int(11) NOT NULL,
+  `IdCompany` int(11) NOT NULL,
+  `IdClient` int(11) NOT NULL,
+  `StartDate` date NOT NULL,
+  `EndDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `client_contract`
+-- Dumping data for table `client_contracts`
 --
 
-LOCK TABLES `client_contract` WRITE;
-/*!40000 ALTER TABLE `client_contract` DISABLE KEYS */;
-INSERT INTO `client_contract` VALUES (1,1,1,'2011-02-02','2012-02-02'),(2,1,2,'2012-02-03','2014-02-02');
-/*!40000 ALTER TABLE `client_contract` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `client_contracts` (`Id`, `IdCompany`, `IdClient`, `StartDate`, `EndDate`) VALUES
+(1, 1, 1, '2011-02-02', '2012-02-02'),
+(2, 1, 2, '2012-02-03', '2014-02-02');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `company`
+-- Table structure for table `companies`
 --
 
-DROP TABLE IF EXISTS `company`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `company` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `companies` (
+  `Id` int(11) NOT NULL,
   `Name` varchar(50) NOT NULL,
-  `Contract_Start_Date` date NOT NULL,
-  `Contract_End_Date` date NOT NULL,
+  `ContractStartDate` date NOT NULL,
+  `ContractEndDate` date NOT NULL,
   `Description` varchar(50) DEFAULT NULL,
-  `User_Name` varchar(225) DEFAULT NULL,
-  `User_Password` varchar(225) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `Username` varchar(225) DEFAULT NULL,
+  `Password` varchar(225) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `company`
+-- Dumping data for table `companies`
 --
 
-LOCK TABLES `company` WRITE;
-/*!40000 ALTER TABLE `company` DISABLE KEYS */;
-INSERT INTO `company` VALUES (1,'Firma1','2011-04-02','2012-04-02','asd','firma1','parola1'),(2,'Firma2','2012-04-05','2014-05-12','asd1','firma2','parola2');
-/*!40000 ALTER TABLE `company` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `companies` (`Id`, `Name`, `ContractStartDate`, `ContractEndDate`, `Description`, `Username`, `Password`) VALUES
+(1, 'Firma1', '2011-04-02', '2012-04-02', 'asd', 'firma1', 'parola1'),
+(2, 'Firma2', '2012-04-05', '2014-05-12', 'asd1', 'firma2', 'parola2');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `employee`
+-- Table structure for table `employees`
 --
 
-DROP TABLE IF EXISTS `employee`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `employee` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `User_Name` varchar(225) DEFAULT NULL,
-  `User_Password` varchar(225) DEFAULT NULL,
-  `First_Name` varchar(50) NOT NULL,
-  `Last_Name` varchar(50) NOT NULL,
+CREATE TABLE `employees` (
+  `Id` int(11) NOT NULL,
+  `Username` varchar(225) DEFAULT NULL,
+  `Password` varchar(225) DEFAULT NULL,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
   `Phone` varchar(50) NOT NULL,
   `Email` varchar(50) NOT NULL,
-  `Rating` double(4,3) NOT NULL DEFAULT '0.000',
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `Rating` double(4,3) NOT NULL DEFAULT '0.000'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `employee`
+-- Dumping data for table `employees`
 --
 
-LOCK TABLES `employee` WRITE;
-/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
-INSERT INTO `employee` VALUES (1,'employee','passemployee1','employee1','employee1','0123012312','employee1@hotmail.com',0.000),(2,'employee','passemployee2','employee2','employee2','012312313','employee2@hotmail.com',0.000);
-/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `employees` (`Id`, `Username`, `Password`, `FirstName`, `LastName`, `Phone`, `Email`, `Rating`) VALUES
+(1, 'employee', 'passemployee1', 'employee1', 'employee1', '0123012312', 'employee1@hotmail.com', 0.000),
+(2, 'employee', 'passemployee2', 'employee2', 'employee2', '012312313', 'employee2@hotmail.com', 0.000);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `employee_contract`
+-- Table structure for table `employee_contracts`
 --
 
-DROP TABLE IF EXISTS `employee_contract`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `employee_contract` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `ID_Company` int(11) NOT NULL,
-  `ID_Employee` int(11) NOT NULL,
-  `Start_Date` date NOT NULL,
-  `End_Date` date NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `Foreign_Key_Company_idx` (`ID_Company`),
-  KEY `Foreign_Key_Employee_idx` (`ID_Employee`),
-  CONSTRAINT `Foreign_Key_Company` FOREIGN KEY (`ID_Company`) REFERENCES `company` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `Foreign_Key_Employee` FOREIGN KEY (`ID_Employee`) REFERENCES `employee` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `employee_contracts` (
+  `Id` int(11) NOT NULL,
+  `IdCompany` int(11) NOT NULL,
+  `IdEmployee` int(11) NOT NULL,
+  `StartDate` date NOT NULL,
+  `EndDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `employee_contract`
+-- Dumping data for table `employee_contracts`
 --
 
-LOCK TABLES `employee_contract` WRITE;
-/*!40000 ALTER TABLE `employee_contract` DISABLE KEYS */;
-INSERT INTO `employee_contract` VALUES (1,1,1,'2010-02-02','2012-02-02'),(2,2,2,'2011-02-02','2011-02-01');
-/*!40000 ALTER TABLE `employee_contract` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `employee_contracts` (`Id`, `IdCompany`, `IdEmployee`, `StartDate`, `EndDate`) VALUES
+(1, 1, 1, '2010-02-02', '2012-02-02'),
+(2, 2, 2, '2011-02-02', '2011-02-01');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `meetings`
 --
 
-DROP TABLE IF EXISTS `meetings`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `meetings` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `ID_Client` int(11) NOT NULL,
-  `ID_Employee` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
+  `IdClient` int(11) NOT NULL,
+  `IdEmployee` int(11) NOT NULL,
   `Date` date NOT NULL,
   `Feedback` int(11) DEFAULT NULL,
-  `Description` mediumtext,
-  PRIMARY KEY (`ID`),
-  KEY `Foreign_Key_Client_idx` (`ID_Client`),
-  KEY `Foreign_Key_Employee_idx` (`ID_Employee`),
-  CONSTRAINT `Foreign_Key_Client1` FOREIGN KEY (`ID_Client`) REFERENCES `client` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `Foreign_Key_Employee1` FOREIGN KEY (`ID_Employee`) REFERENCES `company` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `Description` mediumtext
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `meetings`
 --
 
-LOCK TABLES `meetings` WRITE;
-/*!40000 ALTER TABLE `meetings` DISABLE KEYS */;
-INSERT INTO `meetings` VALUES (1,1,1,'2010-01-01',5,'da'),(2,1,2,'2010-04-02',3,'nu');
-/*!40000 ALTER TABLE `meetings` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `meetings` (`Id`, `IdClient`, `IdEmployee`, `Date`, `Feedback`, `Description`) VALUES
+(1, 1, 1, '2010-01-01', 5, 'da'),
+(2, 1, 2, '2010-04-02', 3, 'nu');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `reports`
 --
 
-DROP TABLE IF EXISTS `reports`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `reports` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `ID_Meeting` int(11) NOT NULL,
-  `Description` text,
-  PRIMARY KEY (`ID`),
-  KEY `Foreign_Key_Meeting_idx` (`ID_Meeting`),
-  CONSTRAINT `Foreign_Key_Meeting` FOREIGN KEY (`ID_Meeting`) REFERENCES `meetings` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `Id` int(11) NOT NULL,
+  `IdMeeting` int(11) NOT NULL,
+  `Description` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `reports`
 --
 
-LOCK TABLES `reports` WRITE;
-/*!40000 ALTER TABLE `reports` DISABLE KEYS */;
-INSERT INTO `reports` VALUES (1,1,'solved'),(2,1,'solv3d');
-/*!40000 ALTER TABLE `reports` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `reports` (`Id`, `IdMeeting`, `Description`) VALUES
+(1, 1, 'solved'),
+(2, 1, 'solv3d');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `client_contracts`
+--
+ALTER TABLE `client_contracts`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `Company_Foreign_Key_idx` (`IdCompany`),
+  ADD KEY `Client_Foreign_Key_idx` (`IdClient`);
+
+--
+-- Indexes for table `companies`
+--
+ALTER TABLE `companies`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `employee_contracts`
+--
+ALTER TABLE `employee_contracts`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `Foreign_Key_Company_idx` (`IdCompany`),
+  ADD KEY `Foreign_Key_Employee_idx` (`IdEmployee`);
+
+--
+-- Indexes for table `meetings`
+--
+ALTER TABLE `meetings`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `Foreign_Key_Client_idx` (`IdClient`),
+  ADD KEY `Foreign_Key_Employee_idx` (`IdEmployee`);
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `Foreign_Key_Meeting_idx` (`IdMeeting`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `client_contracts`
+--
+ALTER TABLE `client_contracts`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `companies`
+--
+ALTER TABLE `companies`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `employee_contracts`
+--
+ALTER TABLE `employee_contracts`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `meetings`
+--
+ALTER TABLE `meetings`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `client_contracts`
+--
+ALTER TABLE `client_contracts`
+  ADD CONSTRAINT `Client_Foreign_Key` FOREIGN KEY (`IdClient`) REFERENCES `clients` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `Company_Foreign_Key` FOREIGN KEY (`IdCompany`) REFERENCES `companies` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `employee_contracts`
+--
+ALTER TABLE `employee_contracts`
+  ADD CONSTRAINT `Foreign_Key_Company` FOREIGN KEY (`IdCompany`) REFERENCES `companies` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `Foreign_Key_Employee` FOREIGN KEY (`IdEmployee`) REFERENCES `employees` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `meetings`
+--
+ALTER TABLE `meetings`
+  ADD CONSTRAINT `Foreign_Key_Client1` FOREIGN KEY (`IdClient`) REFERENCES `clients` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `Foreign_Key_Employee1` FOREIGN KEY (`IdEmployee`) REFERENCES `companies` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `reports`
+--
+ALTER TABLE `reports`
+  ADD CONSTRAINT `Foreign_Key_Meeting` FOREIGN KEY (`IdMeeting`) REFERENCES `meetings` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-05-10  0:51:01
