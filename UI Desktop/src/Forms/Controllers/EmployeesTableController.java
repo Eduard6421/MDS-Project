756f6e15989e903c455950492e2c58c2f599df57
AlinaMaican <36770213+AlinaMaican@users.noreply.github.com>
2018-05-20 20:13:46 +0300

    Added EmployeesTableController

---
+++ UI Desktop/src/Forms/Controllers/EmployeesTableController.java
@@ -0,0 +1,114 @@
+package Forms.Controllers;
+
+
+import Controllers.EmployeesController;
+import Forms.EmployeesTable;
+import Models.Employee;
+import Utils.Converters;
+import Utils.GlobalData;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.util.Pair;
+
+public class EmployeesTableController implements ActionListener {
+    
+    private EmployeesTable form;
+    
+    private boolean focus = true;
+    
+    private CompanyMenuController parentController;
+    
+    
+    
+    
+    public EmployeesTableController() {
+    }
+    
+    public EmployeesTableController(CompanyMenuController parentController) throws SQLException {
+        
+        this.parentController = parentController;
+        this.parentController.setWindowInvisible();
+        
+        form = new EmployeesTable();
+        form.setVisible(true);
+        
+        
+        fillTable();
+    }
+
+    EmployeesTableController(CompanyMenuController aThis, String command) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent evt) {     
+
+        String command = evt.getActionCommand();
+        
+        if (focus) {
+            switch (command) {
+                case "Back":
+                    form.setVisible(false);
+                    form.dispose();
+                    parentController.setWindowVisible();
+                    break;
+            }
+        }   
+    }
+   
+    
+
+    public void toggleFocus() {
+        focus = !focus;
+    }
+
+    public void setWindowInvisible() {
+        form.setVisible(false);
+    }
+
+    public void setWindowVisible() {
+        form.setVisible(true);
+    }
+
+    public void closeWindow() {
+        form.setVisible(false);
+        form.dispose();
+    }
+    
+    public void fillTable() throws SQLException {
+        
+        List<Object[]> rows = new ArrayList<>();
+        
+        List<Employee> employees = new ArrayList<>();
+        
+        employees = (List<Employee>) EmployeesController.getAll();
+        
+        for (Employee employee : employees) {
+            
+            Object[] row = new Object[6];
+            row[0] = employee.getUsername();
+            row[1] = employee.getFirstName();
+            row[2] = employee.getLastName();
+            row[3] = employee.getPhone();
+            row[4] = employee.getEmail();
+            row[5] = employee.getRating();
+            
+            rows.add(row);       
+        }
+          
+        form.showPopulation(rows);
+        
+    }
+}
+    
+   
+        
+    
+    
+
+
